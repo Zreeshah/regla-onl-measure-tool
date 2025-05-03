f6d42897aafe1e8a1e8891df045747a264b3cbf5
Zreeshah <seokprojects1@gmail.com>
2025-05-03 19:22:00 +0500

    Update prerender.js

--- prerender.js
+++ prerender.js
@@ -12,15 +12,15 @@ const routesToPrerender = fs
   .readdirSync(toAbsolute('src/pages'))
   .map((file) => {
     const name = file.replace(/\.tsx$/, '').toLowerCase()
-    return name === 'index' ? '/' : `/${name}`
+    return name === 'index' ? '/' : /${name}
   })
 
 ;(async () => {
   for (const url of routesToPrerender) {
-    const appHtml = await render(url); // ✅ Added `await` here
+    const appHtml = render(url);
     const html = template.replace('<!--app-html-->', appHtml)
 
-    const filePath = `dist${url === '/' ? '/index' : url}.html`
+    const filePath = dist${url === '/' ? '/index' : url}.html
     fs.writeFileSync(toAbsolute(filePath), html)
     console.log('pre-rendered:', filePath)
   }
